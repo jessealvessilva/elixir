d839949048c227683991e740546e7748a75c7b5b
Jessé Augusto <jessealvessilva@gmail.com>
2023-04-30 20:52:39 -0300

    Desmembrando a função com guard

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -10,11 +10,22 @@
   def handle_file_read({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map( &String.to_integer/1 )
+    |> Enum.map( &convert_and_valuate_numbers / 1  )
     # Chamada simplifica de função anônima
 
   end
 
   def handle_file_read({:error, reason}) , do:  "Erro reading the file #{ reason}"
+
+  def convert_and_valuate_numbers(elem) do
+    number = String.to_integer( elem)
+    evaluate_numbers( number)
+
+  end
+
+  def evaluate_numbers(number) when rem( number, 3 ) == 0 and rem( number, 3 ) == 0 ,  do: :fizzbuzz
+  def evaluate_numbers(number) when rem( number, 3 ) == 0,  do: :fizz
+  def evaluate_numbers(number) when rem( number, 5 ) == 0,  do: :buzz
+  def evaluate_numbers(number) ,  do: number
 
 end
